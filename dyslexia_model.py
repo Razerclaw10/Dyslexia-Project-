a47a3b0bd53dcbeaf23d5d4b6ec846a30d4c5916
Razerclaw10 <razerclaw10@gmail.com>
2025-01-27 00:40:42 -0800

    Update dyslexia_model.py
    changed model #

--- dyslexia_model.py
+++ dyslexia_model.py
@@ -75,7 +75,7 @@ class DyslexiaDetector:
 
         # Split and scale the data
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
 
         X_train_scaled = self.scaler.fit_transform(X_train)
 
